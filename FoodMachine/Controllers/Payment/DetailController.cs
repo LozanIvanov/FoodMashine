fc467304517346e4e6c46a23440d71d192bd4e13
unknown <Lozan.ivanov@abv.bg>
2025-08-30 21:44:15 +0300

    create detail page controler and view

---
+++ FoodMachine/Controllers/Payment/DetailController.cs
@@ -0,0 +1,40 @@
+﻿using Food.Dal.Models.Admin;
+using Food.Dal.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Web.Controllers
+{
+    [Route("payment/detail")]
+    public class DetailController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public DetailController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index(int id)
+        {
+            var product = _productService.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ProductViewModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Discription,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                MainImage = product.MainImage
+            };
+
+            return View("~/Views/Payment/Detail.cshtml", model);
+        }
+    }
+}
